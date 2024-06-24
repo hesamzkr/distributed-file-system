c4d05e7f234fdc45d4ee64abdffcd3883dfb79fa
hesamzkr <hesamaldinzakeri@gmail.com>
2024-06-24 12:00:27 +0200

    fix port and docker compose

--- helenite/master/__main__.py
+++ helenite/master/__main__.py
@@ -112,7 +112,7 @@ class MasterServicer(core_pb2_grpc.MasterServicer):
     async def check_chunkservers(self):
         for server in self.chunkservers:
             try:
-                address = f"{server}:50051"
+                address = f"{server}:50052"
                 async with grpc.aio.insecure_channel(address) as channel:
                     stub = core_pb2_grpc.ChunkServerStub(channel)
                     await stub.Heartbeat(Empty())
